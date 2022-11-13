27c787599fea2787e57b19e4ceb6a659b289cc1b
David Stumpe <dstumpe264@gmail.com>
2022-11-13 16:33:16 -0500

    filled out start-screen div, started js,

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
-// start quiz
+// start quiz 
 // start timer
+// event listener when the button is clicked
+addEventListener("click", startQuiz);
+
+function startQuiz() {
+    // hide start screen
+    
+    // start time
+
+    // display questions
+
+    return null;
+}
+
 // display questions and answers
 // submit button
 // determine right or wrong and return response
