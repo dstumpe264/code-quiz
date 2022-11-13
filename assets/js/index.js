0f28006f3978f27d717e67c35aaa653ae440c410
David Stumpe <dstumpe264@gmail.com>
2022-11-13 10:30:55 -0500

    stubbed out js to comments

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// start quiz
+// start timer
+// display questions and answers
+// submit button
+// determine right or wrong and return response
+// next question
+// display final score
+// ask for initials
+// display topscores
+// ask to play again
