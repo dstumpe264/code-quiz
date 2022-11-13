14bbe0201ec00b4f056ff13e69981eeae7c9e2cb
David Stumpe <dstumpe264@gmail.com>
2022-11-13 17:20:29 -0500

    created functionality for startQuiz and getQuestion functions, created stub for questions array objects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
-var startButton = document.getElementById('start-button')
+var startButton = document.getElementById('start-button');
+var questionsDiv = document.getElementById('questions');
+var timer = document.getElementById('timer');
+var choices = document.getElementById('choices');
+var submitButton = document.getElementById('submit-button');
 
 // start quiz 
 // start timer
@@ -12,12 +16,32 @@ function startQuiz() {
     // start time
 
     // display questions
+    questionsDiv.removeAttribute('class');
 
-    return null;
+    getQuestion();
+
 }
 
-// display questions and answers
-// submit button
+function getQuestion(){
+    // display questions and answers
+    // submit button
+    var currentQuestion = questions[0];
+    console.log(questions);
+    console.log(currentQuestion);
+    var questionTitle = document.getElementById("title");
+    questionTitle.textContent = currentQuestion.title;
+
+    // loop through choices for current questions
+    currentQuestion.choices.forEach(element => {
+        console.log(element);
+        var choice = element;
+        var choiceBtn = document.createElement('button');
+        choiceBtn.setAttribute('class', 'choice');
+        choiceBtn.setAttribute('value', choice);
+        choiceBtn.textContent = choice;
+        choices.appendChild(choiceBtn);
+    });
+}
 // determine right or wrong and return response
 // next question
 // display final score
---
+++ assets/js/questions.js
@@ -0,0 +1,8 @@
+// array of question objects
+var questions = [
+    {
+        title: "title",
+        choices: ['a'],
+        answer: 'a'
+    },
+]
